15db4f91cbc7d2c352af69206c189a5db84e90f6
Kira <wordandbloom@gmail.com>
2016-04-02 21:54:21 +0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,8 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
   var items = document.querySelectorAll('.mover');
   var phase;
-  var newScroll = document.body.ScrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    phase = Math.sin(newScroll + (i % 5)) * 100;
+    phase = Math.sin((document.body.ScrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
   // User Timing API to the rescue again. Seriously, it's worth learning.
