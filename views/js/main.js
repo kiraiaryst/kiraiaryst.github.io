d96aa8313409372afd49008526cac65a309f03ba
Kira <wordandbloom@gmail.com>
2016-04-08 20:50:04 +0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -545,7 +545,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza.jpg";
     elem.style.height = "100px";
     elem.style.width = "100px";
     elem.basicLeft = (i % cols) * s;
